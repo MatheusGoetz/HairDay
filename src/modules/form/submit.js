b8f67b12733f484c1661a1f3115cb023b5388f4d
Matheus Goetz <goetz.dev@gmail.com>
2025-06-20 18:30:48 -0300

    criando e configurando o api-config e criando serviço de agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,9 @@
 import dayjs from "dayjs"
 
+import {scheduleNew} from "../../services/schedule-new.js"
+
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -8,8 +11,39 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault()
 
-  console.log("Enviado!")
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value.trim()
+    if(!name){
+      alert("Informe o nome do cliente!")
+    }
+
+    // Recuperar o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected")
+    if(!hourSelected){
+      return alert("Selecione a hora.")
+    }
+
+    // Recupera somente a hora.
+    const [hour] = hourSelected.innerText.split(":")
+
+    // Inserir a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+    
+    // Gera um id
+    const id = new Date().getTime()
+
+    await scheduleNew({
+      id,
+      name,
+      when,
+    })
+
+  } catch (error) {
+    alert("Não foi possivel realizar o agendamento")
+    console.log(error)
+  }
 }
